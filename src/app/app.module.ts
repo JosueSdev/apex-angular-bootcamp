6edb90578d446890d43e55b99b1dfa3c64a4461e
JosueSdev <josue.sandoval@bigcommerce.com>
2024-03-22 15:09:05 -0600

    move into it's own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CapitalizeDirective } from './directives/capitalize.directive';
+import { CustomDirectivesComponent } from './components/custom-directives/custom-directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CapitalizeDirective
+    CapitalizeDirective,
+    CustomDirectivesComponent
   ],
   imports: [
     BrowserModule,
